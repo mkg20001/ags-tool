f45d07c5bfcc45826cfcff72d54c6dbdbd0f1c17
Maciej Krüger <mkg20001@gmail.com>
2019-09-17 00:49:47 +0200

    feat: add pad admin page

---
+++ public/vue/pages/pad-admin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ $t('padadmin.title') }}</h1>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Erstellt am</th>
+          <th style="width: 8px;" scope="col"><i class="fas fa-link"></i></th>
+          <th style="width: 8px;" scope="col"><i class="fas fa-trash"></i></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="pad in pads">
+          <th scope="row">{{pad.id}}</th>
+          <td>{{pad.createdAt}}</td>
+          <td><a :href="'/pad/' + pad.id"><i class="fas fa-link"></i></a></td>
+          <td><a href="#" onclick="deletePad(pad.id)"><i class="fas fa-trash"></i></a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+  export default {
+    name: 'pad-admin',
+    data: () => ({
+      pads: []
+    }),
+  }
+</script>
