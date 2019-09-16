c16f1a5fc9884cfdad04594157cfe72752306816
Maciej Krüger <mkg20001@gmail.com>
2019-09-17 00:58:06 +0200

    feat: rewrite settings page

--- public/vue/pages/settings.vue
+++ public/vue/pages/settings.vue
@@ -1,30 +1,29 @@
 <template>
-  <div>
-    <center class="page-title-mini">
-      <h1>{{ $t('settings.title') }}</h1>
+  <div style="text-align: left; max-width: 512px; margin: auto;">
+    <br>
+    <h1>{{ $t('settings.title') }}</h1>
 
-      <div class="page-title-mini-inner">
-        <form>
-          <h2>{{ $t('settings.lang') }}</h2>
+    <div class="lower-layer">
+      <form>
+        <h2>{{ $t('settings.lang') }}</h2>
 
-          <div v-for="(lang, i) in langs" class="lower-layer radio">
-            <label>
-              <input v-model="$i18n.locale" id="i18n" type="radio" :key="`Lang${i}`" :value="lang"> <!--  name="optionsRadios" id="optionsRadios1" value="option1" checked -->
-              {{ $t('settings.locale.' + lang) }}
-            </label>
-          </div>
+        <div v-for="(lang, i) in langs" class="lower-layer">
+          <label>
+            <input v-model="$i18n.locale" id="i18n" type="radio" :key="`Lang${i}`" :value="lang"> <!--  name="optionsRadios" id="optionsRadios1" value="option1" checked -->
+            {{ $t('settings.locale.' + lang) }}
+          </label>
+        </div>
 
-          <h2>{{ $t('settings.dark') }}</h2>
+        <h2>{{ $t('settings.dark') }}</h2>
 
-          <div class="lower-layer checkbox">
-            <label>
-              <input type="checkbox" v-model="$ui.dark">
-              {{ $t('settings.dark_desc') }}
-            </label>
-          </div>
-        </form>
-      </div>
-    </center>
+        <div class="lower-layer">
+          <label>
+            <input type="checkbox" v-model="$ui.dark">
+            {{ $t('settings.dark_desc') }}
+          </label>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
