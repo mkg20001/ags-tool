9bc5d91f0ebabb099020c90d6507bd5dc46a67bf
Maciej Krüger <mkg20001@gmail.com>
2019-09-19 20:44:10 +0200

    feat: initial da pad

--- public/vue/pages/pad.vue
+++ public/vue/pages/pad.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
     <h1>{{ $t('pad.title') }}</h1>
+    <div id="dapad"></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#dapad {
+  min-height: 100vh;
+  overflow-y: scroll;
+}
 </style>
 
 <script>
   export default {
     name: 'pad',
     data: () => ({}),
+    mounted () {
+      const padId = this.$route.params.id
+      const authorId = this.$user.id || 'g' + String(Math.random()).replace(/[^1-9]/g, '').substr(0, 6)
+
+      const client = require('da-pad/src/client')
+
+      client(
+        {
+          authorId,
+          padId
+        },
+        {
+          htmlField: '#dapad'
+        },
+        {
+          padServer: window.location.host,
+          serverAuth: 'TODOADDAUTH'
+        },
+        client.localStorage(padId))
+    }
   }
 </script>
