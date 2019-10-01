8a1b9dbe09682db4fc9c41a29f7cfa0207210df3
Maciej Krüger <mkg20001@gmail.com>
2019-10-01 02:19:07 +0200

    feat: profile page restyle

--- public/vue/pages/profile.vue
+++ public/vue/pages/profile.vue
@@ -5,21 +5,19 @@
     <h5>{{ $t('profile.desc') }}</h5>
     <br>
 
-    <br>
     <h2>{{ $t('profile.name') }}</h2>
     <div class="lower-layer">
       <label>
-        <input type="text" v-model="$user.display">
-        <a class="btn btn-outline-secondary no-border" onclick="updateUser('display')"><i class="fas fa-check"></i></a>
+        <input style="display: inline-block" class="f f-input" type="text" v-model="$user.display">
+        <a style="display: inline-block" class="btn btn-outline-secondary no-border" onclick="updateUser('display')"><i class="fas fa-check"></i></a>
       </label>
     </div>
 
-    <br>
     <h2>{{ $t('profile.email') }}</h2>
     <div class="lower-layer">
       <label>
-        <input type="text" v-model="$user.email">
-        <a class="btn btn-outline-secondary no-border" onclick="updateUser('email')"><i class="fas fa-check"></i></a>
+        <input style="display: inline-block" class="f f-input" type="text" v-model="$user.email">
+        <a style="display: inline-block" class="btn btn-outline-secondary no-border" onclick="updateUser('email')"><i class="fas fa-check"></i></a>
       </label>
     </div>
   </div>
